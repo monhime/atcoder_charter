2d5028216da7352ed88c5624e08f335a5ed24e90
charter <shounane2311000kouzu@icloud.com>
2022-04-12 15:10:39 +0900

    244

---
+++ ABC/244/A.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, N) for (int i = 0; i < (int)N; i++)
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define ALL(x) (x).begin(), (x).end()
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pill = pair<int, ll>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  int n;
+  string s;
+  cin >> n;
+  cin >> s;
+
+  cout << s[s.length() - 1] << "\n";
+
+  return 0;
+}

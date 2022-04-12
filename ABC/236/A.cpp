c55ef2c761bda32e806b2b43ae2be569a7228332
charter <shounane2311000kouzu@icloud.com>
2022-04-12 14:06:16 +0900

    236

---
+++ ABC/236/A.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, N) for (int i = 0; i < (int)N; i++)
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define ALL(x) (x).begin(), (x).end()
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pill = pair<int, ll>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  string s;
+  cin >> s;
+  int a, b;
+  cin >> a >> b;
+  a -= 1;
+  b -= 1;
+
+  string s2 = s.substr(0, a) + s[b] + s.substr(a + 1, b - a - 1) + s[a] +
+              s.substr(b + 1);
+
+  cout << s2 << "\n";
+  return 0;
+}

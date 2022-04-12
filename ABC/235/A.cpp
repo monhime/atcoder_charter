f0439b184aea75314b1c805cf7860ff8a80e430c
charter <shounane2311000kouzu@icloud.com>
2022-04-12 13:55:14 +0900

    235

---
+++ ABC/235/A.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, N) for (int i = 0; i < (int)N; i++)
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define ALL(x) (x).begin(), (x).end()
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pill = pair<int, ll>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  string s;
+  cin >> s;
+  const char* v_c = s.c_str();
+
+  int a = v_c[0] - '0';
+  int b = v_c[1] - '0';
+  int c = v_c[2] - '0';
+  cout << 111 * (a + b + c) << "\n";
+
+  return 0;
+}

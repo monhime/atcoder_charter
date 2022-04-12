92c83a594d3771af94d0924e26e258b7e043fd3f
charter <shounane2311000kouzu@icloud.com>
2022-04-12 15:55:03 +0900

    246a

---
+++ ABC/246/A.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, N) for (int i = 0; i < (int)N; i++)
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define ALL(x) (x).begin(), (x).end()
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pill = pair<int, ll>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  vi X(3), Y(3);
+  REP(i, 3) cin >> X[i] >> Y[i];
+  sort(ALL(X));
+  sort(ALL(Y));
+  int x, y;
+  if (X[1] == X[0])
+    x = X[2];
+  else
+    x = X[0];
+  if (Y[1] == Y[0])
+    y = Y[2];
+  else
+    y = Y[0];
+
+  cout << x << " " << y << "\n";
+  return 0;
+}

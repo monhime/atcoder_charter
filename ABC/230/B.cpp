47d4bb77576b9e7b0ff93c4a213384729a33d7db
charter <shounane2311000kouzu@icloud.com>
2022-04-13 11:43:39 +0900

    230b

---
+++ ABC/230/B.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, N) for (int i = 0; i < (int)N; i++)
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pill = pair<int, ll>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  string s;
+  cin >> s;
+  string t = "";
+  REP(i, 5) { t += "oxx"; }
+  int s_len = s.length();
+  if ((t.substr(0, s_len) == s) || (t.substr(1, s_len) == s) ||
+      (t.substr(2, s_len) == s)) {
+    cout << "Yes\n";
+  } else {
+    cout << "No\n";
+  }
+
+  return 0;
+}

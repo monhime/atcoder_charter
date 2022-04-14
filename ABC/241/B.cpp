a4cdf6931dda3d71b1de90e3ab1cb837dc9f0cc4
charter <shounane2311000kouzu@icloud.com>
2022-04-14 10:00:04 +0900

    241b

---
+++ ABC/241/B.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, N) for (int i = 0; i < (int)N; i++)
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+using ll = long long;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n, m;
+  cin >> n >> m;
+  unordered_map<int, int> map_A;
+  int a;
+  REP(i, n) {
+    cin >> a;
+    map_A[a]++;
+  }
+  int b;
+  REP(i, m) {
+    cin >> b;
+    if (map_A[b]) {
+      map_A[b]--;
+    } else {
+      cout << "No\n";
+      return 0;
+    }
+  }
+  cout << "Yes\n";
+
+  return 0;
+}

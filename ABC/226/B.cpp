01832b64dd5f818911b057f9020949a373b12f5e
charter <shounane2311000kouzu@icloud.com>
2022-04-14 14:22:02 +0900

    225b

--- ABC/226/B.cpp
+++ ABC/226/B.cpp
@@ -9,54 +9,38 @@ constexpr int MOD = 1000000007;
 constexpr long long llinf = 1LL << 62;
 constexpr int inf = 1LL << 30;
 constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
-
 using ll = long long;
-using Pii = pair<int, int>;
-using Pill = pair<int, ll>;
-using Pll = pair<ll, ll>;
-using vi = vector<int>;
-using vb = vector<bool>;
-using vvi = vector<vi>;
-using vll = vector<ll>;
-using vvll = vector<vll>;
-
-template <class T>
-bool chmax(T& a, const T& b) {
-  if (a < b) {
-    a = b;
-    return 1;
-  }
-  return 0;
-}
-template <class T>
-bool chmin(T& a, const T& b) {
-  if (b < a) {
-    a = b;
-    return 1;
-  }
-  return 0;
-}
-int S(const int& a, const int& b) { return 4 * a * b + 3 * a + 3 * b; }
 
 int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
   int n;
   cin >> n;
-  unordered_set<int> uSet;
-  int temp_s;
-  FOR(a, 1, 140) {
-    FOR(b, 1, 140) {
-      temp_s = S(a, b);
-      if (temp_s > 1000) {
-        continue;
-      }
-      uSet.insert(temp_s);
+  vector<vector<int>> A(n);
+  vector<int> L(n);
+  int a;
+  REP(i, n) {
+    cin >> L[i];
+    REP(j, L[i]) {
+      cin >> a;
+      A[i].emplace_back(a);
     }
   }
-  int ans = 0;
-  REP(i, n) {
-    cin >> s;
-    if (uSet.count(s) == 0) {
+  sort(ALL(A));
+
+  int ans = 1;
+  REP(i, n - 1) {
+    int j = 0;
+    if (A[i].size() != A[i + 1].size()) {
       ans++;
+      continue;
+    }
+    REP(j, A[i].size()) {
+      if (A[i][j] != A[i + 1][j]) {
+        ans++;
+        break;
+      }
     }
   }
   cout << ans << "\n";

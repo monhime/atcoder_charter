a36525132a4b2c14ba38cc593798686e950f4f5a
charter <shounane2311000kouzu@icloud.com>
2022-04-17 08:33:58 +0900

    248ab

---
+++ ABC/248/A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, N) for (int i = 0; i < (int)N; i++)
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+using ll = long long;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  string s;
+  cin >> s;
+  vector<bool> F(10, false);
+  REP(i, s.length()) { F[s[i] - '0'] = true; }
+  REP(i, 10) {
+    if (!F[i]) {
+      cout << i << "\n";
+    }
+  }
+
+  return 0;
+}

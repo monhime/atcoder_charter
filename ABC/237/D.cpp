b023e15479738fe5c14005ead5d03844a224e96d
charter <shounane2311000kouzu@icloud.com>
2022-04-21 14:25:53 +0900

    237d

---
+++ ABC/237/D.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, N) for (int i = 0; i < (int)N; i++)
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+using ll = long long;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n;
+  string s;
+  cin >> n >> s;
+  deque<int> dq_s;
+  dq_s.emplace_back(n);
+  for (int i = n - 1; i >= 0; i--) {
+    if (s[i] == 'R') {
+      dq_s.emplace_front(i);
+    } else {
+      dq_s.emplace_back(i);
+    }
+  }
+  while (dq_s.size()) {
+    cout << dq_s.front() << " ";
+    dq_s.pop_front();
+  }
+  cout << "\n";
+
+  return 0;
+}

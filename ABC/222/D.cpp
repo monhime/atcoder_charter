b5058cfaba8d46fda5b5b3d38128183f3ea3d9c8
charter <shounane2311000kouzu@icloud.com>
2022-04-27 09:58:20 +0900

    222d

---
+++ ABC/222/D.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x) cerr << #x << ": " << x << '\n'
+using ll = long long;
+constexpr int MOD = 998244353;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+  vector<int> A(n), B(n);
+  for (int i = 0; i < n; i++) {
+    cin >> A[i];
+  }
+  for (int i = 0; i < n; i++) {
+    cin >> B[i];
+  }
+
+  vector<int> dp_sum(3001, 1), dp_tmp(3001);
+  for (int i = 0; i < n; i++) {
+    fill(dp_tmp.begin(), dp_tmp.end(), 0);
+    int a = A[i];
+    int b = B[i];
+    for (int j = a; j < b + 1; j++) {
+      dp_tmp[j] = dp_sum[j];
+    }
+    dp_sum[0] = dp_tmp[0];
+    for (int j = 1; j < 3001; j++) {
+      dp_sum[j] = (dp_sum[j - 1] + dp_tmp[j]) % MOD;
+    }
+  }
+  cout << dp_sum.back() << "\n";
+
+  return 0;
+}

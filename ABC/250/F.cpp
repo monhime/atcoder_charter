a95bf2100d933d09dcb8c88600fc9c319650d6d5
charter <shounane2311000kouzu@icloud.com>
2022-05-08 20:50:42 +0900

    250

---
+++ ABC/250/F.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define debug(x) std::cerr << #x << ": " << x << '\n'
+
+using ll = long long;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+
+  return 0;
+}

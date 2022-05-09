f50bd11a161d6df314860b70c1c8bf5a72e9fd76
charter <shounane2311000kouzu@icloud.com>
2022-05-09 10:26:13 +0900

    250a

--- ABC/250/A.cpp
+++ ABC/250/A.cpp
@@ -7,5 +7,14 @@ int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
 
+  int h, w, r, c;
+  std::cin >> h >> w >> r >> c;
+  int ans = 4;
+  if (r == 1) ans--;
+  if (r == h) ans--;
+  if (c == 1) ans--;
+  if (c == w) ans--;
+  std::cout << ans << "\n";
+
   return 0;
 }

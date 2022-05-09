8e7680d8357ab955dc9335f94f001d8df3b8c360
charter <shounane2311000kouzu@icloud.com>
2022-05-09 10:26:20 +0900

    250b

--- ABC/250/B.cpp
+++ ABC/250/B.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #define debug(x) std::cerr << #x << ": " << x << '\n'
 
 using ll = long long;
@@ -7,5 +8,21 @@ int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
 
+  int n, a, b;
+  std::cin >> n >> a >> b;
+  bool left_white = true, white_flag;
+  for (int tile_tate = 0; tile_tate < n; tile_tate++) {
+    for (int i = 0; i < a; i++) {
+      white_flag = left_white;
+      for (int tile_yoko = 0; tile_yoko < n; tile_yoko++) {
+        for (int j = 0; j < b; j++) {
+          std::cout << (white_flag ? "." : "#");
+        }
+        white_flag ^= true;
+      }
+      std::cout << "\n";
+    }
+    left_white ^= true;
+  }
   return 0;
 }

4b96ef931c9fa44be11af111f09ff1d792305454
charter <shounane2311000kouzu@icloud.com>
2022-12-16 11:45:08 +0900

    261A

---
+++ ABC/261/A.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, N) for (int i = 0; i < (int)N; i++)
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) std::cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+
+int main() {
+    int l1, l2, r1, r2;
+    cin >> l1 >> r1 >> l2 >> r2;
+    if (l1 > l2){
+        swap(l1, l2);
+        swap(r1, r2);
+    }
+    if (r1 <= l2){
+        cout << 0 << "\n";
+    }
+    else{
+        cout << min(r1, r2) - l2 << "\n";
+    }
+
+    return 0;
+}

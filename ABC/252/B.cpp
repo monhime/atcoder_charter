2e5577ddb3a755a92f1b2257da0c144e45733027
charter <shounane2311000kouzu@icloud.com>
2022-12-17 23:00:29 +0900

    252A

---
+++ ABC/252/B.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, N) for (int i = 0; i < (int)N; i++)
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) std::cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+
+int main() {
+    int n, k, b;
+    cin >> n >> k;
+    vi A(n);
+    vector<bool> A_max(n, false);
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+    int max_A = *max_element(ALL(A));
+    for (int i = 0; i < n; i++)
+    {
+        if (A[i] == max_A)
+        {
+            A_max[i] = true;
+        }
+    }
+    
+
+    for (int i = 0; i < k; i++)
+    {
+        cin >> b;
+        if (A_max[b - 1]){
+            cout << "Yes\n";
+            return 0;
+        }
+    }
+    cout << "No\n";
+
+    return 0;
+}

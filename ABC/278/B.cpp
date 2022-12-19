77eec3bed5bcb4acf48aa3e3c4384e74585a82d2
charter <shounane2311000kouzu@icloud.com>
2022-12-19 22:50:43 +0900

    278B

---
+++ ABC/278/B.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+using namespace std;
+#define REP(i, N) for (int i = 0; i < (int)N; i++)
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) std::cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+bool check(int h, int m) {
+  int new_h = h / 10 * 10 + m / 10;
+  int new_m = h % 10 * 10 + m % 10;
+  if ((new_h >= 24) || (new_m >= 60)) {
+    return false;
+  }
+  return true;
+}
+
+int main() {
+  ios::sync_with_stdio(false);  // std::cinとscanf()を混ぜない
+  std::cin.tie(nullptr);        // インタラクティブ実行不可
+
+  int h, m;
+  cin >> h >> m;
+
+  while (true) {
+    if (check(h, m)) {
+      cout << h << " " << m << "\n";
+      return 0;
+    }
+    // 1分進める
+    m++;
+    if (m == 60) {
+      h++;
+      m = 0;
+      if (h == 24) {
+        h = 0;
+      }
+    }
+  }
+
+  return 0;
+}

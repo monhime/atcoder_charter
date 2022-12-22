3fd9a6b028a4112f956e46d9d89af2ee7f695747
charter <shounane2311000kouzu@icloud.com>
2022-12-22 13:37:45 +0900

    264c

---
+++ ABC/264/C2.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, N) for (int i = 0; i < (int)N; i++)
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) std::cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  string s;
+  cin >> s;
+  if (s == "atcoder") {
+    cout << "0\n";
+    return 0;
+  }
+
+  queue<string> Q;
+  Q.emplace("atcoder");
+  map<string, int> map_sedai;
+  map_sedai["atcoder"] = 0;
+  while (!Q.empty()) {
+    string node = Q.front();
+    Q.pop();
+    for (int i = 0; i < s.size() - 1; i++) {
+      string new_node = node;
+      swap(new_node[i], new_node[i + 1]);
+      if (map_sedai[new_node]) {
+        continue;
+      }
+      map_sedai[new_node] = map_sedai[node] + 1;
+      if (new_node == s) {
+        cout << map_sedai[new_node] << "\n";
+        return 0;
+      }
+      Q.emplace(new_node);
+    }
+  }
+
+  return 0;
+}

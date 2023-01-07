52bb99ea3504d941048525e9da9bc2824b343814
charter <shounane2311000kouzu@icloud.com>
2023-01-07 10:15:17 +0900

    144a

---
+++ ARC/144/A.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) std::cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(false);  // std::cinとscanf()を混ぜない
+  cin.tie(nullptr);             // インタラクティブ実行不可
+
+  int n;
+  cin >> n;
+  cout << 2 * n << "\n";
+  int sum_a = 2 * n;
+  string ans = "";
+  while (sum_a) {
+    if (sum_a >= 8) {
+      sum_a -= 8;
+      ans += '4';
+    } else {
+      ans += char('0' + sum_a / 2);
+      break;
+    }
+    // cout << sum_a << "\n";
+  }
+  reverse(ALL(ans));
+  cout << ans << "\n";
+
+  return 0;
+}

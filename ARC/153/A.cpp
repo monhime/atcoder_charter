5fe5103b2e48d40ca86b4a88c5b2ad71e30f5ee2
charter <shounane2311000kouzu@icloud.com>
2023-01-17 08:30:44 +0900

    arc153a

---
+++ ARC/153/A.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) std::cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vb = vector<bool>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(false);  // std::cinとscanf()を混ぜない
+  cin.tie(nullptr);             // インタラクティブ実行不可
+
+  int n;
+  cin >> n;
+  int ans = 100000 + n - 1;
+  string s = to_string(ans);
+  string ans_s = "";
+  ans_s += s[0];
+  ans_s += s[0];
+  ans_s += s[1];
+  ans_s += s[2];
+  ans_s += s[3];
+  ans_s += s[3];
+  ans_s += s[4];
+  ans_s += s[5];
+  ans_s += s[4];
+  cout << ans_s << "\n";
+
+  return 0;
+}

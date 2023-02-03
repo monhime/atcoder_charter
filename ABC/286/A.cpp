5965f721e4bb53078715fbc21c3e525769d0770e
charter <shounane2311000kouzu@icloud.com>
2023-02-03 17:56:21 +0900

    ABC286A

---
+++ ABC/286/A.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) std::cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vb = vector<bool>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(false);  // std::cinとscanf()を混ぜない
+  cin.tie(nullptr);             // インタラクティブ実行不可
+
+  int n, p, q, r, s;
+  cin >> n >> p >> q >> r >> s;
+
+  vi A(n);
+  for (int i = 0; i < n; i++) {
+    cin >> A[i];
+  }
+  for (int i = 1; i <= n; i++) {
+    if (i >= p && i <= q) {
+      cout << A[i - p + r - 1] << " ";
+    } else if (i >= r && i <= s) {
+      cout << A[i - r + p - 1] << " ";
+    } else {
+      cout << A[i - 1] << " ";
+    }
+  }
+  cout << "\n";
+
+  return 0;
+}

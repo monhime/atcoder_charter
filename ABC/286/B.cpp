9726771cb79e696f97c768ddf8538a2f4b44f7f3
charter <shounane2311000kouzu@icloud.com>
2023-02-07 11:24:44 +0900

    286B

---
+++ ABC/286/B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) std::cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vb = vector<bool>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(false);  // std::cinとscanf()を混ぜない
+  cin.tie(nullptr);             // インタラクティブ実行不可
+
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  int i = 0;
+  while (i < n) {
+    if (i + 1 < n && s[i] == 'n' && s[i + 1] == 'a') {
+      cout << "nya";
+      i += 2;
+    } else {
+      cout << s[i];
+      i++;
+    }
+  }
+  cout << "\n";
+
+  return 0;
+}

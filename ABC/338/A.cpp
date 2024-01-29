d0bc3acee6e06e9d81a06da1b3905c0c18820639
charter <shounane2311000kouzu@icloud.com>
2024-01-29 17:30:04 +0900

    338A

---
+++ ABC/338/A.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) std::cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vb = vector<bool>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(false);  // std::cinとscanf()を混ぜない
+  cin.tie(nullptr);             // インタラクティブ実行不可
+
+  string s;
+  cin >> s;
+  if (s[0] < 'A' || s[0] > 'Z') {
+    cout << "No\n";
+    return 0;
+  }
+  for (int i = 1; i < s.size(); i++) {
+    if (s[i] < 'a' || s[i] > 'z') {
+      cout << "No\n";
+      return 0;
+    }
+  }
+  cout << "Yes\n";
+
+  return 0;
+}

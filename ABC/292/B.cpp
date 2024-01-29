26196eb321e6491a6146194bc62f816cbd4cc5d4
charter <shounane2311000kouzu@icloud.com>
2024-01-29 18:48:42 +0900

    292B

---
+++ ABC/292/B.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) std::cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vb = vector<bool>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(false);  // std::cinとscanf()を混ぜない
+  cin.tie(nullptr);             // インタラクティブ実行不可
+
+  int n, q, c, x;
+  cin >> n >> q;
+  vi Status(n, 0);
+  for (int qq = 0; qq < q; qq++) {
+    cin >> c >> x;
+    if (c == 1) {
+      Status[x - 1]++;
+    } else if (c == 2) {
+      Status[x - 1] += 2;
+    } else {
+      if (Status[x - 1] >= 2) {
+        cout << "Yes\n";
+      } else {
+        cout << "No\n";
+      }
+    }
+  }
+
+  return 0;
+}

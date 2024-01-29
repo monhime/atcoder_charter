4f04668d3d1e32b0108b47104d6d7e1996e34caf
charter <shounane2311000kouzu@icloud.com>
2024-01-29 17:27:09 +0900

    309A

---
+++ ABC/309/A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) std::cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vb = vector<bool>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(false);  // std::cinとscanf()を混ぜない
+  cin.tie(nullptr);             // インタラクティブ実行不可
+
+  int a, b;
+  cin >> a >> b;
+  if ((a - 1) / 3 != (b - 1) / 3) {
+    cout << "No\n";
+    return 0;
+  }
+
+  a = (a - 1) % 3 + 1;
+  b = (b - 1) % 3 + 1;
+  if (b == a + 1) {
+    cout << "Yes\n";
+  } else {
+    cout << "No\n";
+  }
+
+  return 0;
+}

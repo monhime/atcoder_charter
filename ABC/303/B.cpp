a3bac25046df791fd8873b341f8a881806768edd
charter <shounane2311000kouzu@icloud.com>
2024-01-30 19:35:10 +0900

    303B

---
+++ ABC/303/B.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) std::cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vb = vector<bool>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(false);  // std::cinとscanf()を混ぜない
+  cin.tie(nullptr);             // インタラクティブ実行不可
+
+  int n, m;
+  cin >> n >> m;
+  vector<vector<bool>> Comb(n, vector<bool>(n, false));
+  vi A(m);
+  for (int i = 0; i < m; i++) {
+    int a;
+    for (int j = 0; j < n; j++) {
+      cin >> a;
+      A[j] = a - 1;
+    }
+    for (int j = 0; j < n; j++) {
+      if (j > 0) {
+        Comb[A[j]][A[j - 1]] = true;
+      }
+      if (j < n - 1) {
+        Comb[A[j]][A[j + 1]] = true;
+      }
+    }
+  }
+  //   for (int i = 0; i < n; i++) {
+  //     for (int j = 0; j < n; j++) {
+  //       cout << Comb[i][j] << " ";
+  //     }
+  //     cout << "\n";
+  //   }
+
+  int ans = 0;
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = i + 1; j < n; j++) {
+      if (!Comb[i][j] && !Comb[j][i]) {
+        ans++;
+      }
+    }
+  }
+  cout << ans << "\n";
+
+  return 0;
+}

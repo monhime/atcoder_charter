57a53d614d7cb2e6f277765d3de0da3657d26d76
charter <shounane2311000kouzu@icloud.com>
2024-02-01 08:42:39 +0900

    335B

---
+++ ABC/335/B.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(x) (x).begin(), (x).end()
+#define debug(x) std::cerr << #x << ": " << x << '\n'
+
+constexpr int MOD = 1000000007;
+constexpr long long llinf = 1LL << 62;
+constexpr int inf = 1LL << 30;
+constexpr int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, -1, 0, 1};
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vb = vector<bool>;
+
+template <class T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+bool chmin(T& a, const T& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(false);  // std::cinとscanf()を混ぜない
+  cin.tie(nullptr);             // インタラクティブ実行不可
+
+  int n;
+  cin >> n;
+
+  for (int x = 0; x < n + 1; x++) {
+    if (x > n) {
+      break;
+    }
+
+    for (int y = 0; y < n + 1; y++) {
+      if (x + y > n) {
+        break;
+      }
+
+      for (int z = 0; z < n + 1; z++) {
+        if (x + y + z > n) {
+          break;
+        }
+        cout << x << " " << y << " " << z << "\n";
+      }
+    }
+  }
+
+  return 0;
+}
